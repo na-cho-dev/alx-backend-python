102af0ca971d0062d26dd902999bd55a5139e5f5
Fortune Iheanacho <nachodev369@gmail.com>
2024-10-10 22:46:00 +0100

    Task 3. Basic annotations - to string

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -3,5 +3,5 @@
 
 
 def to_str(n: float) -> str:
-	""" Returns the string representation of the float. """
-	return (str(n))
+    """ Returns the string representation of the float. """
+    return (str(n))
