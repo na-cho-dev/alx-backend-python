7618319d90bc118232d09c1514ddb3abf24ad5d9
Fortune Iheanacho <nachodev369@gmail.com>
2024-10-10 22:33:34 +0100

    Task 0. Lockboxes

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,9 +1,11 @@
- #!/usr/bin/env python3
+#!/usr/bin/env python3
 """
-Function to adds two float and returns their sum
+    Basic annotations for variables.
 """
 
 
 def add(a: float, b: float) -> float:
-    """Returns the sum of a and b parameter"""
-    return (a + b)
+    """
+        Returns sum of two floats.
+    """
+    return a + b
