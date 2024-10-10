8dc16bbdbc0e62c2aa2acb273c6c56c4fd849fbf
Fortune Iheanacho <nachodev369@gmail.com>
2024-10-10 22:07:55 +0100

    Task 0. Basic annotations - add

---
+++ 0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Function to adds two float and returns their sum
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Returns the sum of a and b parameter"""
+    return (a + b)
