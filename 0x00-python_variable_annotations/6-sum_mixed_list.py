7c06b2ea46a2710f34b15cc5887dbdb471c11599
Fortune Iheanacho <nachodev369@gmail.com>
2024-10-11 00:43:19 +0100

    Task 6. Complex types - mixed list

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Complex types - mixed list"""
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[int, float]) -> float:
+    """Returns the sum of mx_lst"""
+    return sum(mxd_lst)
