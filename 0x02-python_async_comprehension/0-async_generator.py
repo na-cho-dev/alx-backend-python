d3056845d2632188ec86ee90e5c2f4525d75b6ee
Fortune Iheanacho <nachodev369@gmail.com>
2024-10-16 14:28:39 +0100

    Task 0. Async Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,10 +8,14 @@ import random
 
 
 async def async_generator() -> AsyncIterator[float]:
+    """
+    Loops 10 times
+    Yield a random number between 0 and 10
+    """
     iter = 1
 
     while iter < 11:
         await asyncio.sleep(1)
-        yield random.uniform(0, 11)
+        yield random.uniform(0, 10)
 
         iter += 1
